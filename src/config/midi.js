570db9d008261acb2c3574c8e198d81c997edbad
Oscar R.C <oscarrc.web@gmail.com>
2020-12-09 17:47:50 +0100

    [MOD] Refactor configuration object

--- src/config/midi.js
+++ src/config/midi.js
@@ -1,422 +1,424 @@
-export const implementation = {
-    osc: {
-        type: {
-          cc: 53,
-          values: [
-                {
-                    value: 0,
-                    label: "Sawtooth"
-                },
-                {
-                    value: 31,
-                    label: "Triangle"
-                },
-                {
-                    value: 62,
-                    label: "Square"
-                },
-                {
-                    value: 93,
-                    label: "VPN"
-                },
-                {
-                    value: 127,
-                    label: "Alt"
-                }
-            ]
-        },
-        shape: {
-          min: 0,
-          max: 127,
-          step: 1,
-          label: "Shape",
-          cc: 54
-        },
-        alt: {
-          min: 0,
-          max: 127,
-          step: 1,
-          label: "Alternate",
-          cc: 55
-        }
+export const osc = {
+    type: {
+        cc: 53,
+        values: [
+            {
+                value: 0,
+                label: "Sawtooth"
+            },
+            {
+                value: 31,
+                label: "Triangle"
+            },
+            {
+                value: 62,
+                label: "Square"
+            },
+            {
+                value: 93,
+                label: "VPN"
+            },
+            {
+                value: 127,
+                label: "Alt"
+            }
+        ]
     },
-    arp: {
-        type: {
-            cc: 117,
-            values: [
-                {
-                    value: 0,
-                    label: "Up"
-                },
-                {
-                    value: 14,
-                    label: "Down"
-                },
-                {
-                    value: 28,
-                    label: "Up-Down"
-                },
-                {
-                    value: 42,
-                    label: "Down-Up"
-                },
-                {
-                    value: 56,
-                    label: "Converge"
-                },
-                {
-                    value: 70,
-                    label: "Diverge"
-                },
-                {
-                    value: 84,
-                    label: "Conv.-Div."
-                },
-                {
-                    value: 98,
-                    label: "Div.-Conv."
-                },
-                {
-                    value: 112,
-                    label: "Random"
-                },
-                {
-                    value: 127,
-                    label: "Stchastic"
-                }
-            ]
-        },
-        scale: {
-            cc: 118,
-            values: [
-                {
-                    value: 0,
-                    label: "Octave"
-                },
-                {
-                    value: 25,
-                    label: "Major Triad"
-                },
-                {
-                    value: 50,
-                    label: "Major Suspended"
-                },
-                {
-                    value: 75,
-                    label: "Major Augumented"
-                },
-                {
-                    value: 100,
-                    label: "Minor Triad"
-                },
-                {
-                    value: 127,
-                    label: "Minor Diminished"
-                }
-            ]
-        },
-        length: {
+    shape: {
+        min: 0,
+        max: 127,
+        step: 1,
+        label: "Shape",
+        cc: 54
+    },
+    alt: {
+        min: 0,
+        max: 127,
+        step: 1,
+        label: "Alternate",
+        cc: 55
+    }
+};
+
+export const arp = {
+    type: {
+        cc: 117,
+        values: [
+            {
+                value: 0,
+                label: "Up"
+            },
+            {
+                value: 14,
+                label: "Down"
+            },
+            {
+                value: 28,
+                label: "Up-Down"
+            },
+            {
+                value: 42,
+                label: "Down-Up"
+            },
+            {
+                value: 56,
+                label: "Converge"
+            },
+            {
+                value: 70,
+                label: "Diverge"
+            },
+            {
+                value: 84,
+                label: "Conv.-Div."
+            },
+            {
+                value: 98,
+                label: "Div.-Conv."
+            },
+            {
+                value: 112,
+                label: "Random"
+            },
+            {
+                value: 127,
+                label: "Stchastic"
+            }
+        ]
+    },
+    scale: {
+        cc: 118,
+        values: [
+            {
+                value: 0,
+                label: "Octave"
+            },
+            {
+                value: 25,
+                label: "Major Triad"
+            },
+            {
+                value: 50,
+                label: "Major Suspended"
+            },
+            {
+                value: 75,
+                label: "Major Augumented"
+            },
+            {
+                value: 100,
+                label: "Minor Triad"
+            },
+            {
+                value: 127,
+                label: "Minor Diminished"
+            }
+        ]
+    },
+    length: {
+        min: 0,
+        max: 127,
+        step: 1,
+        label: "Length",
+        cc: 119
+    }
+};
+
+export const amp = {
+    type: {
+        cc: 14,
+        values: [
+            {
+                value: 0,
+                label: "ADSR"
+            },
+            {
+                value: 31,
+                label: "AHR"
+            },
+            {
+                value: 62,
+                label: "AR"
+            },
+            {
+                value: 93,
+                label: "AR Loop"
+            },
+            {
+                value: 127,
+                label: "Open"
+            }
+        ]
+    },
+    eg: {
+        attack: {
             min: 0,
             max: 127,
             step: 1,
-            label: "Length",
-            cc: 119
+            label: "Attack",
+            cc: 16
+        },
+        release: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Release",
+            cc: 19
+        },
+    },
+    trem: {
+        depth: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Depth",
+            cc: 20
+        },
+        rate: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Rate",
+            cc: 21
         }
     },
-    amp: {
-        type: {
-            cc: 14,
-            values: [
-                {
-                    value: 0,
-                    label: "ADSR"
-                },
-                {
-                    value: 31,
-                    label: "AHR"
-                },
-                {
-                    value: 62,
-                    label: "AR"
-                },
-                {
-                    value: 93,
-                    label: "AR Loop"
-                },
-                {
-                    value: 127,
-                    label: "Open"
-                }
-            ]
+    lfo: {
+        rate: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Rate",
+            cc: 24
         },
-        eg: {
-            attack: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Attack",
-                cc: 16
-            },
-            release: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Release",
-                cc: 19
-            },
-        },
-        trem: {
-            depth: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Depth",
-                cc: 20
-            },
-            rate: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Rate",
-                cc: 21
-            }
-        },
-        lfo: {
-            rate: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "LFO Rate",
-                cc: 24
-            },
-            depth: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "LFO Depth",
-                cc: 26
-            }
-        }
-    },
-    effects: {
-        mod: {
-            type: {
-                cc: 88,
-                values: [
-                    {
-                        value: 0,
-                        label: "Off"
-                    },
-                    {
-                        value: 25,
-                        label: "Chorus"
-                    },
-                    {
-                        value: 50,
-                        label: "Ensemble"
-                    },
-                    {
-                        value: 75,
-                        label: "Phaser"
-                    },
-                    {
-                        value: 100,
-                        label: "Flanger"
-                    },
-                    {
-                        value: 127,
-                        label: "Random"
-                    }
-                ]
-            },
-            time: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Time",
-                cc: 28
-            },
-            depth: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Depth",
-                cc: 29
-            }
-        },
-        delay: {
-            type: {
-                cc: 89,
-                values: [
-                    {
-                        value: 0,
-                        label: "Off"
-                    },
-                    {
-                        value: 25,
-                        label: "Stereo"
-                    },
-                    {
-                        value: 50,
-                        label: "Mono"
-                    },
-                    {
-                        value: 75,
-                        label: "Ping Pong"
-                    },
-                    {
-                        value: 100,
-                        label: "Hight Pass"
-                    },
-                    {
-                        value: 127,
-                        label: "Tape"
-                    }
-                ]
-            },
-            time: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Time",
-                cc: 30
-            },
-            depth: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Depth",
-                cc: 31
-            },
-            mix: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Mix",
-                cc: 33
-            }
-        },
-        reverb: {
-            type: {
-                cc: 90,
-                values: [
-                    {
-                        value: 0,
-                        label: "Off"
-                    },
-                    {
-                        value: 25,
-                        label: "Hall"
-                    },
-                    {
-                        value: 50,
-                        label: "Plate"
-                    },
-                    {
-                        value: 75,
-                        label: "Space"
-                    },
-                    {
-                        value: 100,
-                        label: "Riser"
-                    },
-                    {
-                        value: 127,
-                        label: "Submarine"
-                    }
-                ]
-            },
-            time: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Time",
-                cc: 34
-            },
-            depth: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Depth",
-                cc: 35
-            },
-            mix: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Mix",
-                cc: 36
-            }
-        }
-    },
-    vcf: {
-        type: {
-            cc: 42,
-            values: [
-                {
-                    value: 0,
-                    label: "LowPass 2p"
-                },
-                {
-                    value: 21,
-                    label: "LowPass 4p"
-                },
-                {
-                    value: 42,
-                    label: "BandPass 2p"
-                },
-                {
-                    value: 63,
-                    label: "BandPass 4p"
-                },
-                {
-                    value: 84,
-                    label: "HightPass 2p"
-                },
-                {
-                    value: 105,
-                    label: "HightPass 4p"
-                },
-                {
-                    value: 127,
-                    label: "Off"
-                }
-            ]
-        },
-        filter: {
-            cutoff: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Cutoff",
-                cc: 43
-            },
-            res: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Resonance",
-                cc: 44
-            },
-        },
-        sweep: {
-            depth: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Depth",
-                cc: 45
-            },
-            rate: {
-                min: 0,
-                max: 127,
-                step: 1,
-                label: "Rate",
-                cc: 46
-            }
+        depth: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Depth",
+            cc: 26
         }
     }
 }
+
+export const effects = {
+    mod: {
+        type: {
+            cc: 88,
+            values: [
+                {
+                    value: 0,
+                    label: "Off"
+                },
+                {
+                    value: 25,
+                    label: "Chorus"
+                },
+                {
+                    value: 50,
+                    label: "Ensemble"
+                },
+                {
+                    value: 75,
+                    label: "Phaser"
+                },
+                {
+                    value: 100,
+                    label: "Flanger"
+                },
+                {
+                    value: 127,
+                    label: "Random"
+                }
+            ]
+        },
+        time: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Time",
+            cc: 28
+        },
+        depth: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Depth",
+            cc: 29
+        }
+    },
+    delay: {
+        type: {
+            cc: 89,
+            values: [
+                {
+                    value: 0,
+                    label: "Off"
+                },
+                {
+                    value: 25,
+                    label: "Stereo"
+                },
+                {
+                    value: 50,
+                    label: "Mono"
+                },
+                {
+                    value: 75,
+                    label: "Ping Pong"
+                },
+                {
+                    value: 100,
+                    label: "Hight Pass"
+                },
+                {
+                    value: 127,
+                    label: "Tape"
+                }
+            ]
+        },
+        time: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Time",
+            cc: 30
+        },
+        depth: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Depth",
+            cc: 31
+        },
+        mix: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Mix",
+            cc: 33
+        }
+    },
+    reverb: {
+        type: {
+            cc: 90,
+            values: [
+                {
+                    value: 0,
+                    label: "Off"
+                },
+                {
+                    value: 25,
+                    label: "Hall"
+                },
+                {
+                    value: 50,
+                    label: "Plate"
+                },
+                {
+                    value: 75,
+                    label: "Space"
+                },
+                {
+                    value: 100,
+                    label: "Riser"
+                },
+                {
+                    value: 127,
+                    label: "Submarine"
+                }
+            ]
+        },
+        time: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Time",
+            cc: 34
+        },
+        depth: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Depth",
+            cc: 35
+        },
+        mix: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Mix",
+            cc: 36
+        }
+    }
+};
+
+export const vcf = {
+    type: {
+        cc: 42,
+        values: [
+            {
+                value: 0,
+                label: "LowPass 2p"
+            },
+            {
+                value: 21,
+                label: "LowPass 4p"
+            },
+            {
+                value: 42,
+                label: "BandPass 2p"
+            },
+            {
+                value: 63,
+                label: "BandPass 4p"
+            },
+            {
+                value: 84,
+                label: "HightPass 2p"
+            },
+            {
+                value: 105,
+                label: "HightPass 4p"
+            },
+            {
+                value: 127,
+                label: "Off"
+            }
+        ]
+    },
+    filter: {
+        cutoff: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Cutoff",
+            cc: 43
+        },
+        res: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Resonance",
+            cc: 44
+        },
+    },
+    sweep: {
+        depth: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Depth",
+            cc: 45
+        },
+        rate: {
+            min: 0,
+            max: 127,
+            step: 1,
+            label: "Rate",
+            cc: 46
+        }
+    }
+};
 
 export const channels = [
     "all",
