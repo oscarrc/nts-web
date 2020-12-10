bf8a83e2b79ae62399661507a8816ad31857c1c7
Oscar R.C <oscarrc.web@gmail.com>
2020-12-10 17:26:22 +0100

    [MOD] Display controls

--- src/components/display.js
+++ src/components/display.js
@@ -4,12 +4,17 @@ import { useSelector } from 'react-redux';
 
 export function Display() { 
     const isLoading = useSelector(state => state.loader).value;
+    const controlValues = useSelector(state => state.synthesizer).value;
+
+    const renderDisplay = (values) => {
+        return <p className="text-lcd text-left">{JSON.stringify(values)}</p>
+    }
 
     return  (
         <Space className="display" direction="vertical">
             { isLoading ? 
                 <h2 className="text-lcd">PLEASE, ALLOW THE APP TO USE YOUR MIDI DEVICES</h2> 
-                : ''
+                : renderDisplay(controlValues)
             }
         </Space>
     );
