da506ce5e080fecc86f36fefd72a6bbb5b143b4c
Oscar R.C <oscarrc.web@gmail.com>
2020-12-11 14:06:15 +0100

    [MOD] Button midi logic

--- src/components/button.js
+++ src/components/button.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
+import { midiControlChange } from '../utils/midi';
 
 import button from '../assets/button.png';
 
 export function Button(props) {
+    const handleChange = useCallback((value) => {
+        value = value === 0 ? props.offValue : props.onValue;
+        midiControlChange(props.cc, value, "", "");
+    },[props.cc, props.offValue, props.onValue]);
+    
     useEffect(() => {
         const element = document.getElementById(props.name + '-btn');
       
         element.value = props.active;
-    })
+
+        element.addEventListener("input", (event)=>{
+            handleChange(event.target.value)
+        });
+    
+        return () => { if (element) element.removeEventListener("input", handleChange) };    
+    },[handleChange, props.active, props.name])
 
     return  (
         <span className="text-light switch-button">
@@ -20,6 +32,7 @@ Button.defaultProps = {
     name: null,
     tag: false,
     cc: null,
-    value: null,
+    onValue: null,
+    offValue: 0,
     active: 0
 };
