c81d125e131729dfdca0b1adfbdad36964c40dbe
Oscar R.C <oscarrc.web@gmail.com>
2020-12-16 17:39:23 +0100

    [FIX] Dropdown select

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -11,33 +11,21 @@ export function Dropdown(props) {
 
     const renderOptions = (opt) => {
         let options = [];
-        opt.forEach( (option) => {
+        opt.forEach( (option, index) => {
             if(option.label !== "Off"){
-                options.push(<Option key={props.cc + option.value} value={option.value}>{option.label}</Option>)
+                options.push(<Option key={props.cc + option.value} value={index}>{option.label}</Option>)
             }
         })
         return options;
     }
 
     const handleChange = useCallback((value) => {
-       if(props.active) midiControlChange(props.cc, value, midiConfig.outputDevice, midiConfig.outputChannel);
+       if(props.active) midiControlChange(props.cc, props.values[value].value, midiConfig.outputDevice, midiConfig.outputChannel);
        if(props.path) dispatch({type:'synthesizer/setControl', payload: pathToStore({}, props.path, value) });
-    },[props.active, props.path, props.cc, midiConfig, dispatch]);
-
-    useEffect( () => {
-        const element = document.getElementById(props.name + props.cc);
-
-        handleChange(element.value)
-
-        element.addEventListener("change", (event)=>{
-          handleChange(event.target.value)
-        });
-    
-        return () => { if(element) element.removeEventListener("change", handleChange) };      
-      }, [handleChange, props.name, props.value, props.cc])
+    },[props.active, props.path, props.cc, props.values, midiConfig, dispatch]);
 
     return  (
-        <Select className="control-select text-lcd" size="medium" id= { props.name + props.cc } name={ props.name } placeholder={ props.name } defaultValue={ props.values[props.value].label }>
+        <Select onChange={handleChange} className="control-select text-lcd" size="medium" id= { props.name + props.cc } name={ props.name } placeholder={ props.name } value={ props.values[props.value].label }>
             { renderOptions(props.values) }
         </Select>
     );
