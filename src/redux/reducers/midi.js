eac03c7390a86d462eae74ce2c8aeb1fa87da32b
Oscar <oscarrc.web@gmail.com>
2020-12-18 19:31:55 +0100

    [MOD] Remove unused reducers

--- src/redux/reducers/midi.js
+++ src/redux/reducers/midi.js
@@ -7,11 +7,8 @@ export const midiSlice = createSlice({
     value: defaultSettings
   },
   reducers: {
-    setOptions: (state, action )=> {
+    setOptions: (state, action) => {
       state.value = { ...state.value, ...action.payload };
-    },
-    setDevices: (state, action) =>{
-      state.value.devices = action.payload;
     },
     toggleSettings: (state) => {
       state.value.settings = !state.value.settings;
@@ -21,6 +18,5 @@ export const midiSlice = createSlice({
 
 export const { setOptions, setDevices, toggleSettings } = midiSlice.actions;
 export const midi = state => state.midi.value;
-export const settings = state => state.midi.value.settings;
 
 export default midiSlice.reducer;
