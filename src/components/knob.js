00e6a7fc5ccd085f59994533019fb9a3a829234e
Oscar R.C <oscarrc.web@gmail.com>
2020-12-21 17:58:12 +0100

    [WIP] fix old midiConfig values or double triggering

--- src/components/knob.js
+++ src/components/knob.js
@@ -1,38 +1,32 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useRef } from 'react';
 import { midiControlChange } from '../utils/midi';
 import { pathToStore } from '../utils/store';
 import { useSelector, useDispatch } from 'react-redux';
 import knob from '../assets/knob.png';
 
-//TODO fix old midiConfig values or double triggering
+//TODO fix old midiConfig values or double triggering (TEST useRef)
 export function Knob(props) {
     const midiConfig = useSelector(state => state.midi).value;
     const dispatch = useDispatch();
-    
-    const handleChange = useCallback((value) => {        
+    const control = useRef(null);
+
+    const handleChange = useCallback((value) => {
         midiControlChange(props.cc, value, midiConfig.outputDevice, midiConfig.outputChannel);
-        if(props.path) dispatch({type:'synthesizer/setControl', payload: pathToStore({}, props.path, value) });
+        if(props.path) dispatch({type:'synthesizer/setControl', payload: pathToStore({}, props.path, value) });  ;
     },[props.cc, props.path, midiConfig, dispatch]);
     
     useEffect( () => {
-      const element = document.getElementById(props.name + props.cc);
-      element.addEventListener("change", (event)=>{
-        handleChange(event.target.value);
-      });
-      
-      return () => { if (element) element.removeEventListener("change", handleChange) };       
-      // eslint-disable-next-line     
-    }, [])
+      const current = control.current;
+      current.addEventListener("input", (event) => handleChange(event.target.value));      
+      return () => current.removeEventListener("input", handleChange);
+    }, [control, handleChange])
 
-    useEffect( () => {
-      const element = document.getElementById(props.name + props.cc);
-      element.value = props.value;
-    }, [props.name, props.cc, props.value])
+    useEffect( () => control.current.value = props.value, [props.value]);
 
     return  (
         <div className='knob-wrapper'>            
           { props.name ? <label className="control-label" htmlFor={ props.name }>{ props.name }</label> : null }       
-          <webaudio-knob class="knob" diameter="60" id={props.name + props.cc} name={props.name} src={knob} step={props.step} min={ props.min } max={ props.max } value={props.value}></webaudio-knob>
+          <webaudio-knob ref={control} class="knob" diameter="60" id={props.name + props.cc} name={props.name} src={knob} step={props.step} min={ props.min } max={ props.max } value={props.value}></webaudio-knob>
           { props.param ? <webaudio-param class="param" link={props.name}></webaudio-param> : null }
         </div>
     );
