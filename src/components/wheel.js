ce393e9cf461b44a398a1cda98fda74c0d7cef1d
Oscar <oscarrc.web@gmail.com>
2021-01-20 19:50:37 +0100

    [MOD] Try to fix wheel [TODO] Make my own wheel since this is not reliable

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -1,24 +1,26 @@
 import React, { useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { midiSendPitchBend } from '../utils/midi';
 import wheel from '../assets/wheel.png';
 
 export function Wheel(props) {
     const control = useRef(null);
-
+    const midiConfig = useSelector(state => state.midi).value;
+    
     useEffect( () => {
         const current = control.current;
         const onEvent = (event) => {
-            midiSendPitchBend(event.target.value);
-            current.value = 0;
+            midiSendPitchBend(event.target.value, midiConfig.outputDevice, midiConfig.outputChannel);
+            current.setValue(0, true);
         }
         current.addEventListener("change", event => onEvent(event))
         return () => current.removeEventListener("change", onEvent)
-    }, [control])
+    }, [midiConfig])
 
     return  (
         <div>
             <webaudio-knob id="pitch-bend" ref={control} sprites="127" width={props.width} height={props.height} src={wheel} step={props.step} min={ props.min } max={ props.max } ></webaudio-knob>
-            <webaudio-param link="pitch-bend"></webaudio-param>
+            <webaudio-param link="pitch-bend" width={props.width} ></webaudio-param>
         </div>
     );
 }
