a0746660d106dc37dfde1c29b3c52c2fae7bbecf
Oscar R.C <oscarrc.web@gmail.com>
2021-01-20 10:47:50 +0100

    [ADD] Dedicated octave selector component

---
+++ src/components/octave.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'antd';
+import { CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
+
+export function Octave() {       
+    const dispatch = useDispatch();
+    const octave = useSelector(state => state.midi).value.octave;
+    const octaveUp = () => dispatch({ type: 'midi/octaveUp'});
+    const octaveDown = () => dispatch({ type: 'midi/octaveDown'});
+
+    return  (
+        <div>
+            <Button disabled={ octave === 10 } onClick={ octaveUp } block ghost className="btn-gold" icon={<CaretUpOutlined />}></Button>
+            <div class="display small"><h2 className="text-lcd">{octave}</h2></div>
+            <Button disabled={ octave === 0 } onClick={ octaveDown } block ghost className="btn-gold" icon={<CaretDownOutlined />}></Button>
+        </div>
+    );
+}
--- src/pages/synth/components/live.js
+++ src/pages/synth/components/live.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, Button } from 'antd';
-import { Keyboard } from '../../../components';
-import { CaretUpOutlined, CaretDownOutlined } from '@ant-design/icons';
+import { useSelector } from 'react-redux';
+import { Row, Col } from 'antd';
+import { Keyboard, Wheel, Octave } from '../../../components';
 
-// TODO octave change control
 // TODO maybe a pitch bend control
 export function Live() {       
-    const dispatch = useDispatch();
     const octave = useSelector(state => state.midi).value.octave;
-    const octaveUp = () => dispatch({ type: 'midi/octaveUp'});
-    const octaveDown = () => dispatch({ type: 'midi/octaveDown'});
 
     return  (
-        <Row justify="space-between" align="center">
-            <Col span={2} justify="center">
-                <Button disabled={ octave === 10 } onClick={ octaveUp } block ghost className="btn-gold" icon={<CaretUpOutlined />}></Button>
-                <div class="display small"><h2 className="text-lcd">{octave}</h2></div>
-                <Button disabled={ octave === 0 } onClick={ octaveDown } block ghost className="btn-gold" icon={<CaretDownOutlined />}></Button>
+        <Row justify="space-around" align="middle">
+            <Col span={2}>
+                <Octave />
             </Col>
             <Col span={18}>
                 <Keyboard octave={octave} />
             </Col>   
-            <Col span={1}>
-                
+            <Col span={2}>
+                <Wheel />
             </Col>
         </Row>
     );
