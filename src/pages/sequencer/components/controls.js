2ed5cac8f437e2bf7cd17ddaf044a0cf18d9197b
Oscar R.C <oscarrc.web@gmail.com>
2021-01-21 14:19:54 +0100

    [MOD] Remove unused icons

--- src/pages/sequencer/components/controls.js
+++ src/pages/sequencer/components/controls.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col, Button, InputNumber  } from 'antd';
-import { CaretRightOutlined, PauseOutlined, RollbackOutlined, RedoOutlined, ArrowRightOutlined } from '@ant-design/icons';
+import { CaretRightOutlined, RollbackOutlined, RedoOutlined } from '@ant-design/icons';
 
 export function Controls(props) {  
     return (
