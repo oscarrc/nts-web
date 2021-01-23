9258f176466512c1f9c7060b83e02d72fcacb852
Oscar <oscarrc.web@gmail.com>
2021-01-23 13:03:25 +0100

    [ADD] Footer disclaimer

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,6 +9,7 @@ export function Footer() {
         <Footer className="footer transparent">
             <Space className="credits" align="center">
                 <p className="text-gold">Made with <HeartFilled className="text-red" /> by <a className="link text-gold" href="https://oscarrc.me" rel="noreferrer" target="_BLANK">Oscar R.C.</a></p>
+                <p className="text-light"><i>This page is not affiliated or endorsed by Korg</i></p>
                 <Space>
                     <Button ghost size="small" className="btn-gold"><a href="https://ko-fi.com/oscarrc" target="_BLANK" rel="noreferrer"><CoffeeOutlined /> Buy me a coffee</a></Button>
                     <Button ghost size="small" className="btn-gold"><a href="https://github.com/oscarrc/nts-web/issues" target="_BLANK" rel="noreferrer"><BugOutlined /></a></Button>
