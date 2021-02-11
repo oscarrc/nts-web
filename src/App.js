5d99910bd078f7300c925138dc63615675c80650
Oscar <oscarrc.web@gmail.com>
2021-02-11 12:28:23 +0100

    [ADD] Synth reducers

--- src/App.js
+++ src/App.js
@@ -3,15 +3,68 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { Layout } from 'antd';
 import { Header, Footer, Settings } from './components/layout';
+import { midiStart, midiListenPassthrough, midiListenControlChange, midiGetUserPrograms } from './utils/midi';
 import { channels } from './config/midi';
 import "antd/dist/antd.css";
 import './App.css';
+
+const scripts = ["assets/js/webaudio-controls.js",  "assets/js/webaudio-pianoroll.js"]
 
 function App(){  
 	const dispatch = useDispatch();
 	const appState = useSelector(state => state.app).value;
 	const midiState = useSelector(state => state.midi).value;
+
+	const initScripts = (scripts) => {
+		scripts.forEach( s => {
+			if(!document.querySelector(`script[src="${s}"`)){
+				const current = document.createElement('script');
+				current.src= s;
+				current.async= true;
+				current.type="text/javascript";
+				document.body.appendChild(current);
+			}
+		})
+	}
+
+	const initMidi = () => {
+		midiStart().then( devices => {
+			dispatch({ type: "midi/setOptions", payload: devices});
+			dispatch({ type: "app/toggleLoading" });
+			
+			if(devices.inputDevice && devices.outputDevice) dispatch({ type: "display/setMessage", payload: "welcome" });
+			else return Promise.reject("nodevice")
+			
+			return midiGetUserPrograms(midiState.inputDevice, midiState.outputDevice, midiState.sysexVendor, midiState.sysexDevice, midiState.sysexChannel);
+		}).then( count => {
+			dispatch({ type: "synth/setUserPrograms", payload: count});			
+		}).catch( err => {
+			dispatch({ type: "display/setMessage", payload: err ? err : "error" });
+		});
+	}
+
+	const initPassthrough = (midi) => midiListenPassthrough(midi.passthorughDevice, midi.pasthroughChannel, midi.outputDevice, midi.outputChannel);
+	const initControlChange = (midi) => midiListenControlChange(midi.inputDevice, midi.inputChannel, (e) => {
+		dispatch({ type: "synth/setControl", payload: {
+			cc: e.data[1],
+			value: e.data[2]
+		}});
+	});
 	
+	useEffect( () => {
+		initScripts(scripts);
+		initMidi();
+	}, [])
+
+	useEffect( () => {
+		initPassthrough(midiState);
+		initControlChange(midiState);
+		return () => { 
+			initPassthrough(midiState);
+			initControlChange(midiState);
+		}
+	}, [midiState])
+
 	return (
 		<Layout id="app">
 			<Header/>
--- src/redux/reducers/synth.js
+++ src/redux/reducers/synth.js
@@ -21,16 +21,37 @@ export const synthSlice = createSlice({
   },
   reducers: {
       setPatch: (state, action) => {
+        state.value.patches[state.value.bank] = action.payload;
       },
       setControl: (state, action) => {
+        state.value.patches[state.value.bank][action.payload.cc] = {
+          ...state.value.patches[state.value.bank][action.payload.cc],
+          ...action.payload.value
+        };
       },
       setBank: (state, action) => {
+        state.value.bank = action.payload;
       },
       setOctave: (state, action) => {
+        state.value.octave = action.payload;
       },
       setUserPrograms: (state, action) => {
+        Object.keys(action.payload).forEach( k => {
+          state.value.patches.forEach( p => {
+            p[k].max = p[k].max + action.payload[k];
+            p[k].step =  Math.round(127/p[k].max);
+          })
+        })
       },
       randomize: (state) => {
+        Object.keys(state.value.patches[state.value.bank]).forEach( k => {          
+          const max = state.value.patches[state.value.bank][k].max || 127;
+          const min = state.value.patches[state.value.bank][k].min;
+          const step = state.value.patches[state.value.bank][k].step || 1;
+		  const value = Math.floor(Math.random() * ((max ? max : 127) - (min ? min : 0) + 1) + (min ? min : 0)) * step
+
+          state.value.patches[state.value.bank][k].value = value < max * step ? value : 127;
+        })
       }
   }
 });
