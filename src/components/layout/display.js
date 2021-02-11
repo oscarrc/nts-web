bd7208c2ef89b7a0a1ab9277fc9f6f8a0e11dec5
Oscar <oscarrc.web@gmail.com>
2021-02-11 13:23:51 +0100

    [MOD] Center display text

--- src/components/layout/display.js
+++ src/components/layout/display.js
@@ -3,9 +3,9 @@ import { Space, Row, Col } from 'antd';
 
 export function Display(props) {
     return  (
-        <Space align="start" className="display bg-grid">
+        <Space className="display bg-grid">
            <Row className="text-lcd" align="middle">
-            <Col span={24}>
+                <Col span={24}>
                     <h2>{props.title}</h2> 
                     <p>{props.text}</p>
                 </Col>  
