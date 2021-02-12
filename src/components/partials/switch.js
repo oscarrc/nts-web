f51808382ffd18abb01ab815ebbb668e14c969f2
Oscar <oscarrc.web@gmail.com>
2021-02-12 10:47:10 +0100

    [ADD] Switch on change function

--- src/components/partials/switch.js
+++ src/components/partials/switch.js
@@ -4,7 +4,17 @@ import button from '../../assets/button.png';
 
 export function Switch(props) {
     const control = useRef(null);
-    
+    const onChange = (event) => props.onChange(props.cc, { active: event.target.value, value: props.value });
+
+    useEffect(() => {
+        const current = control.current;
+        current.addEventListener("change", onChange);  
+        if( current.value !== props.value){
+            current.value = props.value;
+        }   
+        return () => current.removeEventListener("change", onChange);   
+    }, [props.value]);
+
     return  (
         <span className="text-light switch-button">
             <webaudio-switch 
