410c3bc382a6dc57aab848f607cbe86ca78370a6
Oscar <oscarrc.web@gmail.com>
2021-02-12 11:11:05 +0100

    [MOD] Remove unused imports

--- src/components/partials/dropdown.js
+++ src/components/partials/dropdown.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Select } from 'antd';
 
 export function Dropdown(props) {
