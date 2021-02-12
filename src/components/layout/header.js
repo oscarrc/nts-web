3952c2bedcfd3b43634e5c19d95b93980048c3f7
Oscar <oscarrc.web@gmail.com>
2021-02-12 16:53:16 +0100

    [MOD] Simplify header export

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { PageHeader, Button, Dropdown, Menu, Space, Upload, Layout } from 'antd';
-import { DownOutlined, LinkOutlined, DownloadOutlined, UploadOutlined, SettingOutlined } from '@ant-design/icons';
+import { PageHeader, Button, Space, Upload, Layout } from 'antd';
+import { DownloadOutlined, UploadOutlined, SettingOutlined } from '@ant-design/icons';
 
 import korg from '../../assets/korg.svg';
 
@@ -10,17 +10,6 @@ export function Header() {
     const dispatch = useDispatch();
 
     const toggleSettings = () => dispatch({type: 'app/toggleSettings'});
-
-    const menu = (
-        <Menu className="menu-dark">
-            <Menu.Item key="copy" icon={<LinkOutlined />}>
-                Copy link
-            </Menu.Item>
-            <Menu.Item key="save" icon={<DownloadOutlined />}>
-                Save file
-            </Menu.Item>
-        </Menu>
-    )
 
     return  (
         <Header className="header transparent">
@@ -32,11 +21,7 @@ export function Header() {
                         <Upload accept=".ntspatch" showUploadList={false} customRequest={ () => false }>
                             <Button ghost className="btn-gold" icon={<UploadOutlined />}>Import</Button>
                         </Upload>
-                        <Dropdown key="menu" overlay={menu}>
-                            <Button ghost className="btn-gold">
-                                Export <DownOutlined />
-                            </Button>
-                        </Dropdown>
+                        <Button ghost className="btn-gold" icon={<DownloadOutlined />}>Export</Button>
                         <Button ghost onClick={toggleSettings} className="btn-gold" icon={<SettingOutlined />}></Button>
                     </Space>
                 ]}
