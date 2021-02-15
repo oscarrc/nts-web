2462e500b4936c36a267b8f045c0476d1ceb1d11
Oscar <oscarrc.web@gmail.com>
2021-02-15 16:27:46 +0100

    [MOD] Display message on patch selected

--- src/components/layout/patches.js
+++ src/components/layout/patches.js
@@ -7,9 +7,7 @@ import { exportData, importData } from '../../utils/files';
 export function Patches(props) {
     const dispatch = useDispatch();
 
-    const setPatch = (bank) => dispatch({ type:"synth/setBank", payload: bank });
     const exportPatch = (bank) => exportData(props.patches[bank], "patch.ntspatch");
-
     const importPatch = async (file, bank) => {
         const patch = await importData(file);
         dispatch({ type: "synth/setPatch", payload: {
@@ -21,6 +19,13 @@ export function Patches(props) {
             text: "Patch has been imported to bank " + (bank + 1)
         }});
     }
+   
+    const setPatch = (bank) => {
+        dispatch({ type:"synth/setBank", payload: bank })
+        dispatch( {type: "display/setDisplay", payload: {
+            title: "Bank " + (bank + 1) + " selected"
+        }});
+    };
 
     const renderButtons = () => {
         let buttons = [];
