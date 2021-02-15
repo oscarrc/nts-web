b13d072c64111fe6bb9d77d1bd0cb0cb2203900b
Oscar <oscarrc.web@gmail.com>
2021-02-15 10:34:11 +0100

    [MOD] Affix display on small screens

--- src/components/layout/display.js
+++ src/components/layout/display.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Space, Row, Col } from 'antd';
+import { Space, Row, Col, Affix, Grid } from 'antd';
 
 export function Display(props) {
+    const { useBreakpoint } = Grid;
+    const screens = useBreakpoint();
+
     return  (
-        <Space className="display bg-grid">
-           <Row className="text-lcd" align="middle">
-                <Col span={24}>
-                    <h2>{props.title}</h2> 
-                    <p>{props.text}</p>
-                </Col>  
-           </Row>
-        </Space>
+        <Affix offsetTop={screens.xs || screens.sm ? 12 : -999999 }>
+            <Space className="display bg-grid">
+                <Row className="text-lcd" align="middle">
+                        <Col span={24}>
+                            <h2>{props.title}</h2> 
+                            <p>{props.text}</p>
+                        </Col>  
+                </Row>
+            </Space>
+        </Affix>
     );
 }
