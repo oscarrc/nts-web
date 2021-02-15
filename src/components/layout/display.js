ebb0bcd3d41ae7ac80ca3a38454ac291953d076d
Oscar <oscarrc.web@gmail.com>
2021-02-15 10:37:34 +0100

    [MOD] indentation

--- src/components/layout/display.js
+++ src/components/layout/display.js
@@ -9,10 +9,10 @@ export function Display(props) {
         <Affix offsetTop={screens.xxl || screens.xl || screens.md ? -999999 : 12 }>
             <Space className="display bg-grid">
                 <Row className="text-lcd" align="middle">
-                        <Col span={24}>
-                            <h2>{props.title}</h2> 
-                            <p>{props.text}</p>
-                        </Col>  
+                    <Col span={24}>
+                        <h2>{props.title}</h2> 
+                        <p>{props.text}</p>
+                    </Col>  
                 </Row>
             </Space>
         </Affix>
