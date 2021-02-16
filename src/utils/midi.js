da4df78972ffd9df72b459470e17353465e5a233
Oscar <oscarrc.web@gmail.com>
2021-02-16 12:20:22 +0100

    [MOD] Send hello message before getting user programs

--- src/utils/midi.js
+++ src/utils/midi.js
@@ -106,7 +106,8 @@ const midiGetUserPrograms = (inputId, outputId, inputChannel, vendor, device, ch
         }
         
         input.addListener("sysex", inputChannel, doCount);        
-        output.sendSysex(vendor, [48 + channel, 0, 1, device, 25, type, bank]);
+        // output.sendSysex(vendor, [48 + channel, 0, 1, device, 25, type, bank]);
+        output.sendSysex(vendor, [80, 0, 2]);
     })
 }
 
