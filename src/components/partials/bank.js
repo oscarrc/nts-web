c2eb0b687de358c2b3ce31872cd36ef462f6c372
Oscar <oscarrc.web@gmail.com>
2021-04-06 12:05:40 +0200

    [WIP] Preliminary native file upload support

--- src/components/partials/bank.js
+++ src/components/partials/bank.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {  Dropdown, Menu, Upload } from 'antd';
 import { DownOutlined, DownloadOutlined, UploadOutlined } from '@ant-design/icons';
+import { Capacitor, Plugins } from '@capacitor/core';
+import 'capacitor-file-chooser';
 
 export function Bank(props) {
+    const { CapacitorFileChooser } = Plugins;
+    const pickFile = async () => {
+        if (Capacitor.isNative){            
+            let file = await CapacitorFileChooser.picker();
+            console.log(file);
+            props.onImport(file, props.bank);
+        }
+    }
+
     const menu = (
         <Menu className="menu-dark">
-            <Menu.Item key="import" icon={<UploadOutlined />}>
+            <Menu.Item key="import" icon={<UploadOutlined />} onClick={pickFile}>
                 <Upload accept={ props.accept } showUploadList={false} beforeUpload={ file => props.onImport(file, props.bank) } customRequest={ () => false }>
                     Import
                 </Upload>
