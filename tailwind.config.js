c8808b875177e28002daec2095fd90d9ea4bd978
Oscar <oscarrc.web@gmail.com>
2022-10-25 12:33:26 +0200

    [ADD] DaisyUI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,17 @@ module.exports = {
     theme: {
       extend: {},
     },
+    daisyui:{
+        themes: [
+            {
+                nts: {
+                    "primary": "#f3cc62",          
+                    "secondary": "#b4b4b4",                             
+                    "accent": "#d32029",                             
+                    "neutral": "#212122", 
+                }
+            }
+        ]
+    },
     plugins: [require("daisyui")]
 }
