5bddb7ebd8830a2eb097773c60a3baedf4412191
Oscar <oscarrc.web@gmail.com>
2022-10-25 12:41:42 +0200

    [ADD] Custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+            dotMatrix: ["Dot Matrix", "monospace"]
+        }
+      },
     },
     daisyui:{
         themes: [
