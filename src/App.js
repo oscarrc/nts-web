3fade1c7e34d50a6a3130c508c4dab1c50a683d4
Oscar <oscarrc.web@gmail.com>
2022-10-25 13:57:53 +0200

    [ADD] Modal provider and Settings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { ModalProvider } from "./hooks/useModal";
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen px-8">
-      <Header />
-      <main className="flex-1 min-h-full">
-        
-      </main>
-      <Footer />
+      <ModalProvider>
+        <Header />
+        <main className="flex-1 min-h-full">
+          
+        </main>
+        <Footer />
+      </ModalProvider>
     </div>
   );
 }
---
+++ src/hooks/useModal.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
+
+const ModalContext = createContext();
+
+const ModalProvider = ({children}) => {
+    const overlay = useRef(null);
+    const [ modal, setModal ] = useState(false); 
+    const [ isFullWidth, setIsFullWidth ] = useState(false);
+    const [ modalContent, setModalContent ] = useState(false);
+    
+    const modalOpen = "fixed top-0 left-0 right-0 bottom-0 min-w-screen min-h-screen bg-base-100 bg-opacity-75 z-50 opacity-100 visible";
+
+    const handleModal = (content = false, fullWidth = false ) => {
+        setModal(content ? true : false);
+        setIsFullWidth(fullWidth);
+        setModalContent(content || null );
+    }  
+    
+    const toggleModal = (event) => {
+        if(event.target === overlay.current || event.key === "Escape") handleModal();
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", toggleModal, false);
+        return () =>  document.removeEventListener("keydown", toggleModal, false);
+    })
+
+    useEffect(() => {
+        if(modal) document.body.classList.add("overflow-hidden");
+        else document.body.classList.remove("overflow-hidden");
+    })
+
+    return (
+        <ModalContext.Provider value={{ handleModal }}>
+            { children }
+            <div ref={overlay} className={`flex items-center justify-center transition-all duration-50 ease-in ${modal ? modalOpen  : 'opacity-0 modal'}`} onClick={ (e) => toggleModal(e) }>
+                <div className={`modal-box ${ isFullWidth && "max-w-none" } transition-all duration-200 ease-in-out overflow-visible bg-transparent shadow-none`}>
+                    { modal ? modalContent : null }
+                </div>
+            </div>
+        </ ModalContext.Provider>
+    )
+}
+
+const useModal = () => {
+    const context = useContext(ModalContext);
+    if(context === undefined) throw new Error("useModal must be used within a ModalProvider")
+    return context;
+}
+
+export { ModalProvider, useModal }
