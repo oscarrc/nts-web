2cf0107817e9306ec255822085bfa5ee702b24d5
Oscar <oscarrc.web@gmail.com>
2022-10-25 13:54:27 +0200

    [MOD] Hover button color and transition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import { SiKofi } from "react-icons/si";
 
 const Footer = () => {
     return (
-        <footer className="footer items-center justify-items-center	 md:justify-between p-2 gap-y-4">
-            <p className="grid-flow-col items-center">Made with <FaHeart className="text-error h-3 w-3 inline" /> by <a href="https://oscarrc.me" rel="noreferrer" target="_BLANK">Oscar R.C.</a></p>
+        <footer className="footer items-center justify-items-center	 md:justify-between px-2 py-4 gap-y-4">
+            <p className="grid-flow-col items-center">Made with <FaHeart className="text-accent h-3 w-3 inline" /> by <a className="hover:text-accent transition-all transition-200" href="https://oscarrc.me" rel="noreferrer" target="_BLANK">Oscar R.C.</a></p>
             <p className="text-secondary italic">This page is not affiliated or endorsed by Korg</p>
             <div className="flex gap-2">
                 <button className="btn btn-primary btn-outline btn-xs"><SiKofi className="h-3 w-3 mr-2" /> Buy me a coffee</button>
