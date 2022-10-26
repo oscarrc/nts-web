87517717fccedcd2eb7dcda753f5a0d4d1029261
Oscar <oscarrc.web@gmail.com>
2022-10-26 14:21:24 +0200

    [ADD] Dropdowns to settings

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,18 +1,31 @@
+import Dropdown from "../components/controls/Dropdown";
+import { useModal } from "../hooks/useModal";
+
 const Settings = () => {
+    const { handleModal } = useModal();
+
     return (
         <div className="card w-full max-w-lg bg-neutral shadow-xl">
             <div className="card-body">
                 <h2 className="card-title divider divider-primary">MIDI Settings</h2>
                     <form className="flex flex-col">
-                        <h3 className="divider text-secondary">Input</h3>                        
                     <div>
-                        <h3 className="divider text-secondary">Output</h3>                        
+                        <h3 className="divider text-secondary">Input</h3>  
+                        <Dropdown label="Device" />
+                        <Dropdown label="Channel" />
                     </div>
                     <div>
-                        <h3 className="divider text-secondary">Passthrough</h3>                        
+                        <h3 className="divider text-secondary">Output</h3>  
+                        <Dropdown label="Device" />
+                        <Dropdown label="Channel" />                      
+                    </div>
+                    <div>
+                        <h3 className="divider text-secondary">Passthrough</h3>  
+                        <Dropdown label="Device" />
+                        <Dropdown label="Channel" />                      
                     </div>
                     <div className="card-actions justify-end gap-2 mt-4">
-                        <button className="btn btn-sm btn-ghost text-secondary">Cancel</button>
+                        <button type="button" onClick={ () => handleModal() } className="btn btn-sm btn-ghost text-secondary">Cancel</button>
                         <button className="btn btn-sm btn-primary btn-outline">Save</button>
                     </div>
                 </form>
