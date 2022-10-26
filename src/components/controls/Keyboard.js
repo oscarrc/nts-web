addd3c22974733c5bac8a8285083b6263196cbaa
Oscar <oscarrc.web@gmail.com>
2022-10-26 17:55:32 +0200

    [WIP] Keyboard

--- src/components/controls/Keyboard.js
+++ src/components/controls/Keyboard.js
@@ -1,7 +1,12 @@
-const Keyboard = ({ octaves = 3 }) => {
+const Keyboard = ({ octave = 1, octaves = 2 }) => {
     return (
-        <section className=""> 
-        </section>
+        <div className="grid overflow-hidden auto-cols-auto grid-rows-none gap-x-1 grid-flow-col"> 
+            {
+                [...Array((octaves*7 + 1)).keys()].map(() => {
+                    return <span className="min-w-full h-32 w-24 min-h-full rounded bg-secondary"></span>
+                })
+            }
+        </div>
     )
 }
 
