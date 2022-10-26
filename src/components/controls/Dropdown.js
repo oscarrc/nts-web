9fd9456b34ddc247b87f0710eff390b9c4e0c07d
Oscar <oscarrc.web@gmail.com>
2022-10-26 14:23:11 +0200

    [WIP] Dropdown

--- src/components/controls/Dropdown.js
+++ src/components/controls/Dropdown.js
@@ -1,6 +1,14 @@
 import { SlArrowDown } from "react-icons/sl";
+import { useState } from "react";
 
-const Dropdown = ({ label }) => {
+const Dropdown = ({ label, defaultSelection, options, onChange }) => {
+    const [ selected, setSelected ] = useState(defaultSelection);
+
+    const handleSelection = (e) => {
+        setSelected(1)
+        // onChange()
+    }
+
     return (
         <div className="input-select flex items-center gap-4">
             { label && <label className="text-secondary text-xs uppercase font-bold" htmlFor={label}>{label}</label> }
@@ -10,8 +18,8 @@ const Dropdown = ({ label }) => {
                     <SlArrowDown className="text-accent absolute top-2 right-2 h-4 w-4" />
                 </button>
                 <ul tabindex="0" class="mt-2 dropdown-content menu shadow-lg bg-neutral text-secondary rounded w-full">
-                    <li className="px-2 py-1 cursor-pointer hover:bg-base-100">Item 1</li>
-                    <li className="px-2 py-1 cursor-pointer hover:bg-base-100">Item 2</li>
+                    <li onClick={ handleSelection(1) } className="px-2 py-1 cursor-pointer hover:bg-base-100">Item 1</li>
+                    <li onClick={ handleSelection(1) } className="px-2 py-1 cursor-pointer hover:bg-base-100">Item 2</li>
                 </ul>
             </div>
         </div>
