1f25da11e0921c6990a79fa5baf03b82f1b229a7
Oscar <oscarrc.web@gmail.com>
2022-10-27 17:51:49 +0200

    [ADD] Children to display [MOD] Grid layout

--- src/components/layout/Display.js
+++ src/components/layout/Display.js
@@ -1,10 +1,10 @@
 import { FaCaretUp } from "react-icons/fa";
 
-const Display = () => {
+const Display = ({ children }) => {
     return (
         <section className="sticky md:relative flex flex-col gap-4 flex-1 h-full min-h-[235px] mx-4 my-2">
             <div  className="flex-1 bg-neutral bg-grid text-dotMatrix rounded text-accent outline outline-base-100 outline-offset-2 outline-2">
-                
+                { children }
             </div>
             <div className="flex gap-4 justify-between px-2">
                 <button className="btn btn-pushable btn-xs"> <FaCaretUp className="h-4 w-4" /> </button>
