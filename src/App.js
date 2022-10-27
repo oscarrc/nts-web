92f8d77a1228ef600207a0d0cece2fa6596d9792
Oscar <oscarrc.web@gmail.com>
2022-10-27 13:59:41 +0200

    [ADD] Live controls transition animation

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useState } from "react";
 
 const App = () => {
   const { enabled } = useMidi();
-  const [ liveControls, setLiveControls ] = useState(false);
+  const [ liveControls, setLiveControls ] = useState(true);
   
   return (
     <div className="flex flex-col min-h-screen max-w-screen overflow-x-hidden xl:px-8 sm:px-4 px-2">
@@ -18,7 +18,7 @@ const App = () => {
         <main className="flex flex-col flex-1 min-h-full justify-between items-center py-8">
           <NTSProvider>              
             <Synth />
-            <Live show={ liveControls } toggle={ () => setLiveControls(l => !l) } />
+            <Live isOpen={ liveControls } toggle={ () => setLiveControls(l => !l) } />
           </NTSProvider>
         </main>
         <Footer />
--- src/views/Live.js
+++ src/views/Live.js
@@ -1,10 +1,14 @@
 import Keyboard from "../components/controls/Keyboard";
+import { MdClose } from "react-icons/md";
 import Octave from "../components/controls/Octave";
 import Wheel from "../components/controls/Wheel";
 
-const Live = () => {
+const Live = ({ isOpen, toggle }) => {
     return (
-        <aside className="bottom-drawer bg-neutral flex flex-row justify-center items-center gap-16 py-8">
+        <aside className={`${isOpen ? 'translate-y-0' : 'translate-y-full'} transition-transform transition-200 bottom-drawer bg-neutral flex flex-row justify-center items-center gap-16 py-8 text-secondary`}>
+            <button onClick={ toggle } className="btn btn-circle btn-sm absolute top-2 right-2 text-secondary">
+                <MdClose className="h-5 w-5" />
+            </button>
             <Wheel autoReturn={true} />
             <Keyboard />
             <Octave />
