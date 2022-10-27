ad5f75332f15460add4ef96f468d9f9f4592c384
Oscar <oscarrc.web@gmail.com>
2022-10-27 19:36:32 +0200

    [MOD] Display menu and buttons [MOD] Octave counter

--- src/components/controls/Octave.js
+++ src/components/controls/Octave.js
@@ -1,13 +1,18 @@
 import { FaCaretDown, FaCaretUp } from "react-icons/fa";
 
-const Octave = ({ value = 1}) => {
+const Octave = ({ octave, setOctave }) => {
+    const handleClick = (q) => {
+        if(q < 0 && octave > 1) setOctave(o => o - 1)
+        if(q > 0 && octave <= 10) setOctave(o => o + 1)
+    }
+
     return (
         <div className="flex flex-col gap-1">            
-            <button className="btn btn-pushable btn-xs rounded-b-none"> <FaCaretUp className="h-4 w-4" /></button>
+            <button onClick={ () => handleClick(1)} className="btn btn-pushable btn-xs rounded-b-none"> <FaCaretUp className="h-4 w-4" /></button>
             <div className="bg-neutral w-full bg-grid text-dotMatrix h-6 leading-6 text-sm text-center text-accent outline outline-base-100 outline-offset-1 outline-1">
-                { value }
+                { octave }
             </div>            
-            <button className="btn btn-pushable btn-xs rounded-t-none"> <FaCaretDown className="h-4 w-4" /></button>
+            <button onClick={ () => handleClick(-1) } className="btn btn-pushable btn-xs rounded-t-none"> <FaCaretDown className="h-4 w-4" /></button>
         </div>
     )
 }
--- src/components/layout/Display.js
+++ src/components/layout/Display.js
@@ -1,16 +1,22 @@
-import { FaCaretUp } from "react-icons/fa";
+import { BsFillCircleFill } from "react-icons/bs";
 
 const Display = ({ children }) => {
     return (
         <section className="sticky md:relative flex flex-col gap-4 flex-1 h-full min-h-[235px] mx-4 my-2">
-            <div  className="flex-1 bg-neutral bg-grid text-dotMatrix rounded text-accent outline outline-base-100 outline-offset-2 outline-2">
+            <div  className="relative flex-1 bg-neutral bg-grid text-dotMatrix rounded text-accent outline outline-base-100 outline-offset-2 outline-2">
                 { children }
+                <nav className="grid grid-cols-4 gap-4 absolute bottom-0 left-0 w-full bg-transparent font-dotMatrix text-sm px-2">
+                    <span className="text-center">Testamento</span>
+                    <span className="text-center">Test</span>
+                    <span className="text-center">adfgads</span>
+                    <span className="text-center">Test</span>
+                </nav>
             </div>
-            <div className="flex gap-4 justify-between px-2">
-                <button className="btn btn-pushable btn-xs"> <FaCaretUp className="h-4 w-4" /> </button>
-                <button className="btn btn-pushable btn-xs"> <FaCaretUp className="h-4 w-4" /> </button>
-                <button className="btn btn-pushable btn-xs"> <FaCaretUp className="h-4 w-4" /> </button>
-                <button className="btn btn-pushable btn-xs"> <FaCaretUp className="h-4 w-4" /> </button>
+            <div className="grid grid-cols-4 gap-4 justify-between">
+                <button className="btn btn-pushable btn-xs"> <BsFillCircleFill className="h-2 w-2" /> </button>
+                <button className="btn btn-pushable btn-xs"> <BsFillCircleFill className="h-2 w-2" /> </button>
+                <button className="btn btn-pushable btn-xs"> <BsFillCircleFill className="h-2 w-2" /> </button>
+                <button className="btn btn-pushable btn-xs"> <BsFillCircleFill className="h-2 w-2" /> </button>
             </div>
         </section>
     )
