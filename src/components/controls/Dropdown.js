0ded406350de4802599e482c45dd96ee9c789d54
Oscar <oscarrc.web@gmail.com>
2022-10-28 10:37:23 +0200

    [FIX] Dropdown overflow on modal [MOD] Dropdown max-height

--- src/components/controls/Dropdown.js
+++ src/components/controls/Dropdown.js
@@ -1,14 +1,10 @@
 import { SlArrowDown } from "react-icons/sl";
 import switchButton from "../../assets/switch.png";
-import { useState } from "react";
 
-const Dropdown = ({ label, defaultSelection, options, switchValue, onChange }) => {
-    const [ selected, setSelected ] = useState(defaultSelection);
-
+const Dropdown = ({ label, selection = 0, options, switchValue, onChange }) => {
     const handleSelection = (i) => {
         document.activeElement.blur()
-        setSelected(i)
-        //options?.[i] && onChange(options?.[i])
+        onChange && onChange(i)
     }
 
     return (
@@ -17,13 +13,13 @@ const Dropdown = ({ label, defaultSelection, options, switchValue, onChange }) =
             { switchValue && <input type="checkbox" className="input-switch" data-diameter="60" data-src={ switchButton } /> }
             <div className="group dropdown flex-1 my-2">                
                 <button type="button" tabIndex="0" className={`relative text-left cursor-pointer bg-grid px-2 py-1 w-full font-dotMatrix rounded text-accent outline outline-base-100 outline-offset-2 outline-1 hover:outline-accent focus:outline-accent`}>
-                    { options ? options?.[selected].label : "No option" }
+                    { options?.length ? options?.[selection]?.label : "No option" }
                     <SlArrowDown className="text-accent absolute top-2 right-2 h-4 w-4" />
                 </button>
-                <ul tabIndex="0" className="mt-2 dropdown-content menu shadow-lg bg-neutral text-secondary rounded w-full">
+                <ul tabIndex="0" className="mt-2 dropdown-content menu shadow-lg bg-neutral text-secondary rounded w-full max-h-[8rem] flex-row overflow-y-scroll">
                     {   
                         options?.map((option, index) => {
-                            return <li key={ index } onClick={ () => handleSelection(index) } className="px-2 py-1 cursor-pointer hover:bg-base-100">{ option.label }</li>
+                            return <li key={ index } onClick={ () => handleSelection(index) } className="px-2 py-1 cursor-pointer hover:bg-base-100 w-full">{ option?.label }</li>
                         })
                     }
                 </ul>
--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,32 +1,66 @@
 import Dropdown from "../components/controls/Dropdown";
+import { channelList } from "../config/midi";
+import { useMidi } from "../hooks/useMidi";
 import { useModal } from "../hooks/useModal";
 
 const Settings = () => {
     const { handleModal } = useModal();
+    const { devices, setDevices, channels, setChannels } = useMidi();
+    
+    const close = (e) => {
+        e.preventDefault();
+        handleModal();
+    }
 
     return (
-        <div className="card w-full max-w-lg bg-neutral shadow-xl">
+        <div className="card w-full max-w-lg bg-neutral shadow-xl overflow-visible">
             <div className="card-body">
                 <h2 className="card-title divider divider-primary">MIDI Settings</h2>
                     <form className="flex flex-col">
                     <div>
-                        <h3 className="divider text-secondary">Input</h3>  
-                        <Dropdown label="Device" />
-                        <Dropdown label="Channel" />
+                        <h3 className="divider text-secondary">Input</h3>
+                        <Dropdown label="Device" 
+                            options={ devices.inputDevices.map( (d, i) => { return { label: d.name, value: i } }) }
+                            selection={ devices.input }
+                            onChange={ (value) => setDevices({ type: "Input", payload: value }) } 
+                        />
+                        <Dropdown 
+                            label="Channel" 
+                            options={ channelList.map( (c, i) => { return { label: c, value: i } }) } 
+                            selection={ channels.input } 
+                            onChange={ (value) => setChannels({ type:"Input", payload: value }) } 
+                        />
                     </div>
                     <div>
                         <h3 className="divider text-secondary">Output</h3>  
-                        <Dropdown label="Device" />
-                        <Dropdown label="Channel" />                      
+                        <Dropdown label="Device" 
+                            options={ devices.outputDevices.map( (d, i) => { return { label: d.name, value: i } }) }
+                            selection={ devices.output }
+                            onChange={ (value) => setDevices({ type: "Output", payload: value }) }
+                        />
+                        <Dropdown 
+                            label="Channel" 
+                            options={ channelList.map( (c, i) => { return { label: c, value: i } }) } 
+                            selection={ channels.output } 
+                            onChange={ (value) => setChannels({ type:"Output", payload: value }) } 
+                        />                     
                     </div>
                     <div>
                         <h3 className="divider text-secondary">Passthrough</h3>  
-                        <Dropdown label="Device" />
-                        <Dropdown label="Channel" />                      
+                        <Dropdown label="Device" 
+                            options={ devices.passthroughDevices.map( (d, i) => { return { label: d.name, value: i } }) }
+                            selection={ devices.passthrough }
+                            onChange={ (value) => setDevices({ type: "Passthrough", payload: value }) }
+                         />
+                        <Dropdown 
+                            label="Channel" 
+                            options={ channelList.map( (c, i) => { return { label: c, value: i } }) } 
+                            selection={ channels.passthrough } 
+                            onChange={ (value) => setChannels({ type:"Passthrough", payload: value }) } 
+                        />                        
                     </div>
                     <div className="card-actions justify-end gap-2 mt-4">
-                        <button type="button" onClick={ () => handleModal() } className="btn btn-sm btn-ghost text-secondary">Cancel</button>
-                        <button className="btn btn-sm btn-primary btn-outline">Save</button>
+                        <button onClick={ close } className="btn btn-sm btn-primary btn-outline">Close</button>
                     </div>
                 </form>
             </div>
