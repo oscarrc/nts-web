1e8519abef34bfc5ece69b33cc8edf9db298eef8
Oscar <oscarrc.web@gmail.com>
2022-10-31 17:09:17 +0100

    [MOD] Check if switch value is a number

--- src/components/controls/Dropdown.js
+++ src/components/controls/Dropdown.js
@@ -8,9 +8,9 @@ const Dropdown = ({ label, value = 0, options, switchValue, isActive, onChange, 
     }
 
     return (
-        <div className={`input-select flex flex-1 px-4 items-center gap-4 ${!switchValue && 'py-1.5'}`}>
+        <div className={`input-select flex flex-1 px-4 items-center gap-4 ${isNaN(switchValue) && 'py-1.5'}`}>
             { label && <label className="text-secondary text-xs uppercase font-bold" htmlFor={label}>{label}</label> }
-            { switchValue && 
+            { !isNaN(switchValue) && 
                 <input type="checkbox" 
                     onChange={ (e) => onSwitch(e.target.checked) } 
                     value={ isActive } 
