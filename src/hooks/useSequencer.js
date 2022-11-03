29958ea0c7dd811e2779a5715ee26fde298e76fc
Oscar <oscarrc.web@gmail.com>
2022-11-03 16:04:05 +0100

    [TODO] useSequencer

---
+++ src/hooks/useSequencer.js
@@ -0,0 +1,15 @@
+const useSequencer = () => {
+    const [clock, setClock] = useState(null);
+    const [tempo, setTempo] = useState(60);
+    const [step, setStep] = useState(0)
+    const [sequence, setSequence] = useState(null)
+
+    return {
+        clock,
+        tempo,
+        sequence,
+        step
+    }
+}
+
+export default useQuencer;
