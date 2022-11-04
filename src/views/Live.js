9d4d4d427f7e3c21e88b4432535f1be3e694ba1d
Oscar <oscarrc.web@gmail.com>
2022-11-04 12:06:38 +0100

    [MOD] Enable whole control view on smaller screens when keyboard is enabled

--- src/views/Live.js
+++ src/views/Live.js
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import Keyboard from "../components/controls/Keyboard";
 import { MdClose } from "react-icons/md";
 import Octave from "../components/controls/Octave";
@@ -8,9 +10,15 @@ import { useMidi } from "../hooks/useMidi";
 const Live = () => {
     const { bottomDrawer, setBottomDrawer } = useLayout();
     const { octave, setOctave, pitchBend } = useMidi();
+    const drawerRef = useRef(null);
+
+    useEffect(() => {
+        if(bottomDrawer) document.body.style.marginBottom = `${drawerRef.current.clientHeight}px`
+        else  document.body.style.marginBottom = null;
+    }, [bottomDrawer])
 
     return (
-        <aside className={`${ bottomDrawer ? 'translate-y-0' : 'translate-y-full'} transition-transform transition-200 bottom-drawer bg-neutral flex flex-row justify-center items-end gap-16 py-6 xl:px-12 md:px-8 sm:px-4 px-2 text-secondary`}>
+        <aside ref={drawerRef} className={`${ bottomDrawer ? 'translate-y-0' : 'translate-y-full'} transition-transform transition-200 bottom-drawer bg-neutral flex flex-row justify-center items-end gap-16 py-6 xl:px-12 md:px-8 sm:px-4 px-2 text-secondary`}>
             <button onClick={ () => setBottomDrawer( d => !d) } className="btn btn-circle btn-sm absolute top-2 right-2 text-secondary">
                 <MdClose className="h-5 w-5" />
             </button>
