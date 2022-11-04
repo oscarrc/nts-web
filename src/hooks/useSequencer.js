f33a4b5f8ad48e3644469177e795947e18fb3960
Oscar <oscarrc.web@gmail.com>
2022-11-04 16:20:05 +0100

    [WIP] Sequencer

--- src/hooks/useSequencer.js
+++ src/hooks/useSequencer.js
@@ -4,12 +4,22 @@ const useSequencer = () => {
     //TODO: implement clock and sequencer
     // input && input.addListener("clock", (e) => console.log(e) )
     // According to the standard, there are 24 MIDI clocks for every quarter note
+
     const [clock, setClock] = useState(null);
-    const [tempo, setTempo] = useState(60);
+    const [tempo, setTempo] = useState(60); // 60bpm
     const [patch, setPatch] = useState(0);
     const [patches, setPatches] = useState([]);
     const [step, setStep] = useState(0);
-    const [sequence, setSequence] = useState(null);
+    const [sequence, setSequence] = useState([
+        {
+            note: "C4",
+            duration: 24 //  1/4 Tiempo -> 96 clocks
+        },
+        {
+            note: "D3",
+            duration: 96 //  1 Tiempo -> 96 clocks
+        }
+    ]);
 
     return {
         clock,
