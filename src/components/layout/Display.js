ac7fcc40386ea509a214c19fb53473d149b9f599
Oscar <oscarrc.web@gmail.com>
2022-11-09 12:24:48 +0100

    [MOD] Move screen selection to display component

--- src/components/layout/Display.js
+++ src/components/layout/Display.js
@@ -1,16 +1,33 @@
 import { BsCaretDownFill, BsCaretUpFill, BsFillCircleFill, BsFillPauseFill, BsPlayFill } from "react-icons/bs";
-import { useEffect, useState } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 
-import Message from "../screens/Message";
 import { messages } from "../../config/display";
 import { useSequencer } from "../../hooks/useSequencer";
 
-const Display = ({ children, bank, octave, devices, midi }) => {
+const Display = ({ bank, octave, devices, midi, screen }) => {
     const isPlaying = false;
     const { input, output, passthrough } = devices;
     const { tempo } = useSequencer();
     const [ message, setMessage ] = useState(null);
     const [ bpmIndicator, setBpmIndicator ] = useState(0)
+
+    const renderScreen = (bank) => {
+        let Screen = () => { return (<div className="flex-1"></div>) }
+        let props = {};
+
+        switch (screen){
+            case "bank":
+                Screen = lazy(() => import('../screens/Bank'));
+                props = { bank }
+                break;
+            default:
+                Screen = lazy(() => import('../screens/Message'));
+                props = { message }
+                break;
+        }
+
+        return <Screen {...props} />
+    }
 
     useEffect(() => {
         if(!midi) return setMessage(messages["midi"]);
@@ -22,32 +39,29 @@ const Display = ({ children, bank, octave, devices, midi }) => {
 
     }, [midi, input, output, passthrough])
 
-    useEffect(() => {
+    useEffect(() => { // TODO: this makes screen flicker
         let interval;
         interval = setInterval(() => setBpmIndicator(b => !b), (60000/tempo)/2 );
         return () => clearInterval(interval);
-    })
+    }, [tempo])
 
     return (
         <section className="sticky md:relative flex flex-col gap-4 flex-1 h-full min-h-[235px] mx-4 my-2">
-            <div  className="flex flex-col flex-1 bg-neutral bg-grid font-sevenSegment text-xl rounded text-accent outline outline-base-100 outline-offset-2 outline-2">
-                {
-                    false ? <Message message={ message }/> :
-                        <>                
-                            <div className="flex justify-between w-full bg-transparent px-2 border-b border-accent">
-                                <div className="text-center">Bank {bank < 10 && 0 }{bank}</div>                       
-                                <div className="text-center">Octave {octave} </div>
-                                <div className={`text-center relative before:absolute before:bg-accent before:rounded-full before:top-3 before:-left-3 before:w-2 before:h-2 ${ bpmIndicator ? 'before:opacity-1': 'before:opacity-0'}`}>{tempo} BPM</div>
-                            </div>
-                            { children }
-                            <nav className="grid grid-cols-4 gap-4 w-full bg-transparent font-sevenSegment text-sm px-2 border-t border-accent">
-                                <span className="text-center">Test</span>
-                                <span className="text-center">Test</span>
-                                <span className="text-center">Test</span>
-                                <span className="text-center">Test</span>
-                            </nav>                        
-                        </>
-                }
+            <div  className="flex flex-col flex-1 bg-neutral bg-grid font-sevenSegment text-xl rounded text-accent outline outline-base-100 outline-offset-2 outline-2">                            
+                <div className="flex justify-between w-full bg-transparent px-2 border-b border-accent">
+                    <div className="text-center">Bank {bank < 10 && 0 }{bank}</div>                       
+                    <div className="text-center">Octave {octave} </div>
+                    <div className={`text-center relative before:absolute before:bg-accent before:rounded-full before:top-3 before:-left-3 before:w-2 before:h-2 ${ bpmIndicator ? 'before:opacity-1': 'before:opacity-0'}`}>{tempo} BPM</div>
+                </div>
+                <Suspense fallback={<div className="flex-1"></div>}>
+                    { renderScreen(bank) }
+                </Suspense>
+                <nav className="grid grid-cols-4 gap-4 w-full bg-transparent font-sevenSegment text-sm px-2 border-t border-accent">
+                    <span className="text-center">Test</span>
+                    <span className="text-center">Test</span>
+                    <span className="text-center">Test</span>
+                    <span className="text-center">Test</span>
+                </nav>
             </div>
             <div className="grid grid-cols-4 gap-4 justify-between">
                 <button className="btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsCaretUpFill className="h-4 w-4" /> </button>
--- src/views/Synth.js
+++ src/views/Synth.js
@@ -1,39 +1,20 @@
-import { Suspense, lazy, useState } from "react";
-
 import Display from "../components/layout/Display";
 import More from "../components/layout/More";
 import Section from "../components/layout/Section"
 import { defaultLayout } from "../config/layout";
 import { useMidi } from "../hooks/useMidi";
 import { useNTS } from "../hooks/useNTS";
+import { useState } from "react";
 
 const Synth = () => {
     const { enabled, input, output, passthrough, octave } = useMidi();
     const { bank } = useNTS();
-    const [ command, setCommand ] = useState("bank");
-
-    const selectScreen = (command) => {
-        let Screen = () => { return (<div className="flex-1"></div>) }
-
-        switch (command){
-            case "bank":
-                Screen = lazy(() => import('../components/screens/Bank'));
-                break;
-            default:
-                break;
-        }
-
-        return <Screen />
-    }
-
+    const [ screen, setScreen ] = useState("bank");
+    
     return (        
         <div className="grid w-full h-full xl:grid-cols-4 md:grid-cols-2 grid-cols-1 grid-rows-auto gap-8 grid-flow-row">
             <div className="flex flex-col">
-                <Display bank={ bank } midi={ enabled } octave={ octave } devices={{ input, output, passthrough }}>
-                    <Suspense fallback={<div className="flex-1"></div>}>
-                        { selectScreen(command) }
-                    </Suspense>
-                </Display>
+                <Display bank={ bank } midi={ enabled } octave={ octave } devices={{ input, output, passthrough }} screen={screen} setScreen={setScreen} />
                 <Section section={defaultLayout.osc} />
                 <Section section={defaultLayout.arp} />
             </div>
