786f5eba54884469e31d4d0466ce7b769c186e97
Oscar <oscarrc.web@gmail.com>
2022-11-09 18:14:12 +0100

    [ADD] Android

---
+++ android/app/src/main/java/me/oscarrc/nts_web/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package me.oscarrc.nts_web.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
