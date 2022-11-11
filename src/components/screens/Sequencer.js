de9f2cf5cb83e2fb849c89dcc3aecafae5fe0315
Oscar <oscarrc.web@gmail.com>
2022-11-11 17:51:08 +0100

    [MOD] Remove border bottom from last step

--- src/components/screens/Sequencer.js
+++ src/components/screens/Sequencer.js
@@ -13,7 +13,7 @@ const Sequencer = ({step, setStep}) => {
     return (
         <div className="sequencer w-full relative flex-1 px-2 max-h-[180px] overflow-y-scroll">
             { [...Array(stepCount).keys()].map(k => (
-                <div onClick={() => setStep(k)} id={`step-${k}`} key={k} className="grid grid-cols-4 border-b border-accent cursor-pointer">
+                <div onClick={() => setStep(k)} id={`step-${k}`} key={k} className={`grid grid-cols-4 ${ k < stepCount - 1 && 'border-b'} border-accent cursor-pointer`}>
                     <div className="flex items-center">{k === step && <BsCaretRightFill className="h-3 w-3 inline"/>} {k < 10 && '0'}{k}</div>
                     <div>C4</div>
                     <div className="text-center">2</div>
