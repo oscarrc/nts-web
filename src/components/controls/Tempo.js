8985ab6551217e63355682bb5c0de1bb144bb5f4
Oscar <oscarrc.web@gmail.com>
2022-11-11 19:26:19 +0100

    [MKOD] Focus input styling

--- src/components/controls/Tempo.js
+++ src/components/controls/Tempo.js
@@ -8,7 +8,7 @@ const Tempo = ({ tempo, onChange }) => {
     return (
         <div className="flex gap-1 h-8 w-36">            
             <button aria-label="BPM down" onClick={ () => setValue(tempo - 1)} className="btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-r-none">-</button>
-            <input onChange={ (e) => setValue(e.target.value) } type="text" value={ tempo } className="bg-neutral w-full text-center bg-grid font-sevenSegment text-2xl text-accent outline outline-base-100 outline-offset-1 outline-1 px-2" />           
+            <input onChange={ (e) => setValue(e.target.value) } type="text" value={ tempo } className="bg-neutral w-full text-center bg-grid font-sevenSegment text-2xl text-accent outline focus:outline-base-100 focus:outline-offset-1 focus:outline-1 focus:border-none border-none focus:ring-0 outline-base-100 outline-offset-1 outline-1 px-2" />           
             <button aria-label="BPM up" onClick={ () => setValue(tempo + 1) } className="btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-l-none">+</button>
         </div>
     )
--- src/components/layout/Memory.js
+++ src/components/layout/Memory.js
@@ -1,7 +1,7 @@
 import { useNTS } from "../../hooks/useNTS";
 
 const Memory = () => {      
-    const { setBank } = useNTS();
+    const { bank, setBank } = useNTS();
 
     const handleClick = (b) => {
         window.navigator.vibrate && window.navigator.vibrate(10);
@@ -13,7 +13,7 @@ const Memory = () => {
             <h2 className="divider divider-primary font-semibold">MEMORY</h2>
             <div className="grid grid-cols-4 grid-rows-4 w-full px-2 gap-2">
                 { [...Array(16).keys()].map((k) => {
-                    return <button aria-label={`Select bank ${k}`} key={k} onClick={() => handleClick(k) } className="btn btn-ghost btn-pushable border-secondary text-secondary btn-xs">Bank {k}</button>
+                    return <button aria-label={`Select bank ${k}`} key={k} onClick={() => handleClick(k) } className={`btn btn-ghost btn-pushable border-secondary text-secondary btn-xs ${bank === k ? "btn-pushed" : ""}`}>Bank {k}</button>
                 }) }
             </div>
         </section>
