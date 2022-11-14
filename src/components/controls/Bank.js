68f16a43134d4e521cdc5be659ab3a61a165488f
Oscar <oscarrc.web@gmail.com>
2022-11-14 17:36:45 +0100

    [WIP] Rename bank context menu as requested in issue #24

--- src/components/controls/Bank.js
+++ src/components/controls/Bank.js
@@ -1,9 +1,10 @@
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const Bank = ({index, name, selected, onClick, onRename}) => {
     const [dropdown, setDropdown] = useState(false);
     const [isRenaming, setIsRenaming] = useState(false)
-    
+    const dropdownRef = useRef(null);
+
     const handleClick = () => {        
         window.navigator.vibrate && window.navigator.vibrate(10);
         onClick(index)
@@ -20,13 +21,30 @@ const Bank = ({index, name, selected, onClick, onRename}) => {
         setIsRenaming(true);
     }
 
+    useEffect( () => {
+        const handleClickOutside = (e) =>  {
+            console.log(e.target === dropdownRef.current)
+            if(e.target !== dropdownRef.current || e.key === "Escape") setDropdown(false);
+        }
+
+        document.addEventListener("click", handleClickOutside);
+        document.addEventListener("keydown", handleClickOutside);
+        document.addEventListener("contextmenu", handleClickOutside);
+
+        return () => {            
+            document.removeEventListener("click", handleClickOutside);
+            document.removeEventListener("keydown", handleClickOutside);
+            document.removeEventListener("contextmenu", handleClickOutside);
+        }
+    }, [])
+
     return (
-        <div className={`dropdown w-full ${dropdown ? 'dropdown-open' : ''}`}>
-            <button aria-label={`Select bank ${name || index}`} key={index} onContextMenu={handleDropdown} onClick={handleClick} className={`btn btn-ghost btn-pushable border-secondary text-secondary btn-xs w-full ${selected ? "btn-pushed" : ""}`}>
+        <div className={`dropdown dropdown-context w-full ${dropdown ? 'dropdown-open' : ''}`}>
+            <button ref={dropdownRef} aria-label={`Select bank ${name || index}`} key={index} onContextMenu={handleDropdown} onClick={handleClick} className={`btn btn-ghost btn-pushable border-secondary text-secondary btn-xs w-full ${selected ? "btn-pushed" : ""}`}>
                 { name ? name : `Bank ${index}`}
             </button>
-            <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-full">
-                <li><button onClick={ handleRename }>Rename</button></li>
+            <ul className="dropdown-content menu p-0 shadow-lg bg-neutral text-secondary rounded min-w-full">
+                <li><button className="btn-sm" onClick={ handleRename }>Rename</button></li>
             </ul>
         </div>
     )
