3bf9e2c9d2608c6e2ff310eb7fd5a691fd6fd78a
Oscar <oscarrc.web@gmail.com>
2022-11-15 10:37:03 +0100

    [FIX] Text overflow on memory buttons

--- src/components/layout/Memory.js
+++ src/components/layout/Memory.js
@@ -8,7 +8,7 @@ const Memory = () => {
             <h2 className="divider divider-primary font-semibold">MEMORY</h2>
             <div className="grid grid-cols-4 grid-rows-4 w-full px-2 gap-2">
                 { [...Array(16).keys()].map((k) => {
-                    return <button aria-label={`Select bank ${k}`} key={k} onClick={() => setBank(k) } className={`btn btn-ghost btn-pushable border-secondary text-secondary btn-xs ${bank === k ? "btn-pushed" : ""}`}>{ bankNames?.[k] || `Bank ${k}`}</button>
+                    return <button aria-label={`Select bank ${k}`} key={k} onClick={() => setBank(k) } className={`block truncate btn btn-ghost btn-pushable border-secondary text-secondary btn-xs ${bank === k ? "btn-pushed" : ""}`}>{ bankNames?.[k] || `Bank ${k}`}</button>
                 }) }
             </div>
         </section>
