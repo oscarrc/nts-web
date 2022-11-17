46cdac81a92dafbc8861809c6bae1d589c161ca0
Oscar <oscarrc.web@gmail.com>
2022-11-17 14:17:45 +0100

    [UNDO] Bank swap via button on sequencer

--- src/components/layout/Memory.js
+++ src/components/layout/Memory.js
@@ -1,21 +1,14 @@
 import { useNTS } from "../../hooks/useNTS";
-import { useSequencer } from "../../hooks/useSequencer";
 
 const Memory = () => {      
     const { bank, setBank, bankNames } = useNTS();
-    const { step, setSequence, isRecording } = useSequencer();
-
-    const handleClick = (v) => {
-        isRecording && setSequence( s => ( {...s, [step]: { ...s[step], bank: v } } ) );
-        setBank(v)
-    }
 
     return (
         <section>
             <h2 className="divider divider-primary font-semibold">MEMORY</h2>
             <div className="grid grid-cols-4 grid-rows-4 w-full px-2 gap-2">
                 { [...Array(16).keys()].map((k) => {
-                    return <button aria-label={`Select bank ${k}`} key={k} onClick={() => handleClick(k) } className={`block truncate btn btn-ghost btn-pushable border-secondary text-secondary btn-xs ${bank === k ? "btn-pushed" : ""}`}>{ bankNames?.[k] || `Bank ${k}`}</button>
+                    return <button aria-label={`Select bank ${k}`} key={k} onClick={() => setBank(k) } className={`block truncate btn btn-ghost btn-pushable border-secondary text-secondary btn-xs ${bank === k ? "btn-pushed" : ""}`}>{ bankNames?.[k] || `Bank ${k}`}</button>
                 }) }
             </div>
         </section>
