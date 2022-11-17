18c1cd954703fa12b839247c447ef1cf889d7527
Oscar <oscarrc.web@gmail.com>
2022-11-17 13:53:44 +0100

    [WIP+ Metronome toggle

--- src/components/controls/Tempo.js
+++ src/components/controls/Tempo.js
@@ -14,10 +14,12 @@ const Tempo = ({ tempo, onChange }) => {
                 <input onChange={ (e) => setValue(e.target.value) } type="text" value={ tempo } className="bg-neutral w-full text-center bg-grid font-sevenSegment text-2xl text-accent outline focus:outline-base-100 focus:outline-offset-1 focus:outline-1 focus:border-none border-none focus:ring-0 outline-base-100 outline-offset-1 outline-1 px-2" />           
                 <button aria-label="BPM up" onClick={ () => setValue(tempo + 1) } className="px-2 btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-l-none"><BsPlus className="h-4 w-4" /></button>
             </div>
-            <label class="cursor-pointer label py-0">
-                <span class="label-text">Sound</span> 
-                <input aria-label="Metronome sound" type="checkbox" class="toggle toggle-secondary checked:toggle-accent focus:border-none border-none focus:ring-0 outline-none toggle-xs" />
-            </label>
+            <div className="form-control">
+                <label className="cursor-pointer label py-0">
+                    <span className="label-text">Sound</span> 
+                    <input aria-label="Metronome sound" type="checkbox" className="toggle toggle-secondary checked:toggle-accent focus:border-none border-none focus:ring-0 outline-none toggle-xs" />
+                </label>
+            </div>
         </div>
     )
 }
