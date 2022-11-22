608054b048fc8c1911d3af80f74f0d25ae646ed2
Oscar <oscarrc.web@gmail.com>
2022-11-22 17:15:46 +0100

    [ADD] Display tooltips

--- src/components/layout/Display.js
+++ src/components/layout/Display.js
@@ -99,12 +99,20 @@ const Display = () => {
             </div>
             <div className="grid grid-cols-8 gap-4 justify-between">
                 <div className="flex col-span-2 gap-1">
-                    <button onClick={handleUp} aria-label="Up" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsCaretUpFill className="h-4 w-4" /> </button>
-                    <button onClick={handleDown} aria-label="Down" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsCaretDownFill className="h-4 w-4" /> </button>
+                    <span class="tooltip tooltip-bottom" data-tip="Step up">
+                        <button onClick={handleUp} aria-label="Up" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsCaretUpFill className="h-4 w-4" /> </button>
+                    </span>
+                    <span class="tooltip tooltip-bottom" data-tip="Step down">
+                        <button onClick={handleDown} aria-label="Down" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsCaretDownFill className="h-4 w-4" /> </button>
+                    </span>
                 </div>
                 <div className="flex col-span-2 gap-1">
-                    <button onClick={removeBar} aria-label="Add bar" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsDash className="h-4 w-4" /> </button>
-                    <button onClick={addBar} aria-label="Remove bar" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsPlus className="h-4 w-4" /> </button>
+                    <span class="tooltip tooltip-bottom" data-tip="Add bar">
+                        <button onClick={removeBar} aria-label="Add bar" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsDash className="h-4 w-4" /> </button>
+                    </span>
+                    <span class="tooltip tooltip-bottom" data-tip="Remove bar">
+                        <button onClick={addBar} aria-label="Remove bar" className="flex-1 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs"> <BsPlus className="h-4 w-4" /> </button>
+                    </span>
                 </div>
                 <button onClick={toggleRecording} aria-label="Toggle Record" className={`col-span-2 btn btn-outline btn-accent btn-xs ${isRecording ? "animate-blink" : ""}`}> <BsFillCircleFill className="h-2 w-2" /> </button>
                 <button onClick={togglePlay} aria-label="Play/Pause" className={`col-span-2 btn btn-ghost btn-pushable border-secondary text-secondary btn-xs ${isPlaying ? "btn-pushed" : ""}`}> { isPlaying ? <BsFillPauseFill className="h-4 w-4"/> : <BsPlayFill className="h-4 w-4" />} </button>
