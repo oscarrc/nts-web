8bff31eec92c97b99a42e637f077437b571c7743
Oscar <oscarrc.web@gmail.com>
2022-11-24 10:25:08 +0100

    [ADD] Download links

--- src/views/modals/Info.js
+++ src/views/modals/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
 
     const [ active, setActive ] = useState(0);
     const sections = ["How it works", "Compatibility", "Installation"]
-
+    console.log(process.env.npm_package_version)
     return(
         <div className="card w-full max-w-lg bg-neutral shadow-xl overflow-visible">
             <div className="card-body gap-4 mt-2 justify-center">
@@ -56,9 +56,9 @@ const Info = () => {
                                     <AiFillWindows className="h-6 w-6 mr-1" /> Windows
                                 </label>
                                 <ul tabIndex="0" className="dropdown-content menu menu-info p-2 shadow rounded p-0 -top-px w-full">
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">64 bits</a></li>
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">32 bits</a></li>
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">Portable</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_win-x64-${process.env.REACT_APP_VERSION}.exe`} target="_BLANK" rel="noreferrer noopener">64 bits</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_win-ia32-${process.env.REACT_APP_VERSION}.exe`} target="_BLANK" rel="noreferrer noopener">32 bits</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_win-${process.env.REACT_APP_VERSION}.exe`} target="_BLANK" rel="noreferrer noopener">Portable</a></li>
                                 </ul>
                             </div>                            
                             <div className="dropdown m-4 flex-1">                                
@@ -66,11 +66,11 @@ const Info = () => {
                                     <SiLinux className="h-6 w-6 mr-1" /> Linux
                                 </label>
                                 <ul tabIndex="0" className="dropdown-content menu menu-secondary p-2 shadow rounded p-0 -top-px w-full">
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">Deb 64 bits</a></li>
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">Deb 32 bits</a></li>
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">RPM 64 bits</a></li>
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">RPM 32 bits</a></li>
-                                    <li><a href="https://github.com/oscarrc/nts-web/releases/latest/download/{asset_name}" target="_BLANK" rel="noreferrer noopener">AppImage</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-x64-${process.env.REACT_APP_VERSION}.deb`} target="_BLANK" rel="noreferrer noopener">Deb 64 bits</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-ia32-${process.env.REACT_APP_VERSION}.deb`} target="_BLANK" rel="noreferrer noopener">Deb 32 bits</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-x64-${process.env.REACT_APP_VERSION}.rpm`} target="_BLANK" rel="noreferrer noopener">RPM 64 bits</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-ia32-${process.env.REACT_APP_VERSION}.rpm`} target="_BLANK" rel="noreferrer noopener">RPM 32 bits</a></li>
+                                    <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-${process.env.REACT_APP_VERSION}.AppImage`} target="_BLANK" rel="noreferrer noopener">AppImage</a></li>
                                 </ul>
                             </div>
                         </div>                        
