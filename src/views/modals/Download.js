eac0b59a6f24cda9c3a20f6692452e00d237da37
Oscar <oscarrc.web@gmail.com>
2022-12-12 09:50:06 +0100

    [FIX] Linux download links

--- src/views/modals/Download.js
+++ src/views/modals/Download.js
@@ -40,7 +40,7 @@ const Download = () => {
                             <ul tabIndex="0" className="dropdown-content menu menu-secondary p-2 shadow rounded p-0 -top-px w-full">
                                 <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-amd64-${process.env.REACT_APP_VERSION}.deb`} target="_BLANK" rel="noreferrer noopener">Deb 64 bits</a></li>
                                 <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-x86_64-${process.env.REACT_APP_VERSION}.rpm`} target="_BLANK" rel="noreferrer noopener">RPM 64 bits</a></li>
-                                <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-${process.env.REACT_APP_VERSION}.AppImage`} target="_BLANK" rel="noreferrer noopener">AppImage</a></li>
+                                <li><a href={`https://github.com/oscarrc/nts-web/releases/latest/download/NTS-web_linux-x86_64-${process.env.REACT_APP_VERSION}.AppImage`} target="_BLANK" rel="noreferrer noopener">AppImage</a></li>
                             </ul>
                         </div>
                     </div>
