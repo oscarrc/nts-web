17ff36bdfc476a1c314ab8232b257cdefbe51446
Oscar <oscarrc.web@gmail.com>
2024-07-26 14:23:37 +0200

    [MOD] Migrate to Vite

---
+++ src/components/controls/Slider.jsx
@@ -0,0 +1,69 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const Slider = ({id, value = 50, min = 0, max = 100, step = 1, defaultValue = 0, autoReturn = false, label, onChange}) => {
+    const [ currentValue, setValue ] = useState(value ? value : (max - min) / 2 );
+    const sliderRef = useRef(null);
+
+    const handleValue = useCallback((e) => {        
+        if (window.navigator.vibrate) window.navigator.vibrate(10);
+        setValue(e.target.value);
+        onChange && onChange(e.target.value);
+    },[onChange])
+
+    const resetValue = useCallback( (e) => {
+        setValue(defaultValue);
+        onChange && onChange(defaultValue);
+    }, [defaultValue, onChange]);
+
+    useEffect(() => { setValue(value) }, [value]);
+
+    useEffect(() => {
+        if(!autoReturn) return;
+        
+        const currentSlider = sliderRef.current;
+        currentSlider.addEventListener("mouseup", resetValue);        
+        currentSlider.addEventListener("mouseleave", resetValue);     
+        currentSlider.addEventListener("touchend", resetValue);     
+        currentSlider.addEventListener("touchcancel", resetValue);  
+
+        return () => {
+            currentSlider.removeEventListener("mouseup", resetValue);        
+            currentSlider.removeEventListener("mouseleave", resetValue);           
+            currentSlider.removeEventListener("touchstart", resetValue);     
+            currentSlider.addEventListener("touchcancel", resetValue); 
+        }
+    }, [autoReturn, resetValue])
+
+    useEffect(() => {
+        const currentSlider = sliderRef.current;
+
+        currentSlider.addEventListener("change", handleValue)        
+        currentSlider.addEventListener("input", handleValue)
+
+        return () => {
+            currentSlider.removeEventListener("change", handleValue)        
+            currentSlider.removeEventListener("input", handleValue)
+        }
+    }, [handleValue])
+
+
+    return (
+        <span className="slider-wrapper flex flex-col items-center justify-center">
+            { label && <label className="text-secondary text-xs uppercase font-bold" htmlFor={label}>{label}</label> }
+            <input 
+                {...(label ? {id: id} : {"aria-label": id}) }
+                ref={sliderRef} 
+                orientation="vertical"
+                type="range"
+                className="slider rounded bg-neutral bg-grid outline outline-base-100 outline-offset-1 outline-1"
+                value={currentValue}
+                min={min}
+                max={max}
+                step={step}
+                onChange={handleValue}
+            />
+        </span>
+    )
+}
+
+export default Slider;
---
+++ src/components/controls/Tempo.jsx
@@ -0,0 +1,65 @@
+import { BsDash, BsPlus } from "react-icons/bs";
+
+const Tempo = ({ tempo, metronome, barLength, onTempoChange, onBarChange, onToggle }) => {
+    const setTempo = (e) => {
+        const value = parseInt(e)
+        if(typeof value === "number" && value > 0) onTempoChange(value)
+    }
+
+    const setBar = (e) => {
+        const value = parseInt(e)
+        if(typeof value === "number" && value > 0) onBarChange(value)
+    }
+
+    const toggleMetronome = (e) => {
+        onToggle(!metronome)
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+            <div>
+                <label className="text-sm">BPM</label>
+                <div className="flex gap-1 h-8 w-36">            
+                    <button aria-label="BPM down" onClick={ () => setTempo(tempo - 1)} className="px-2 btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-r-none"><BsDash className="h-4 w-4" /></button>
+                    <input
+                        aria-label="BPM"
+                        type="text" 
+                        value={ tempo } 
+                        onChange={ (e) => setTempo(e.target.value) }  
+                        className="bg-neutral w-full text-center bg-grid font-sevenSegment text-2xl text-accent outline focus:outline-base-100 focus:outline-offset-1 focus:outline-1 focus:border-none border-none focus:ring-0 outline-base-100 outline-offset-1 outline-1 px-2"
+                    />           
+                    <button aria-label="BPM up" onClick={ () => setTempo(tempo + 1) } className="px-2 btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-l-none"><BsPlus className="h-4 w-4" /></button>
+                </div>
+            </div>
+            <div>
+                <label className="text-sm">Bar length</label>
+                <div className="flex gap-1 h-8 w-36">            
+                    <button aria-label="Increase bar length" onClick={ () => setBar(barLength - 1)} className="px-2 btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-r-none"><BsDash className="h-4 w-4" /></button>
+                    <input
+                        aria-label="Bar length"
+                        type="text" 
+                        value={ barLength } 
+                        onChange={ (e) => setBar(e.target.value) }  
+                        className="bg-neutral w-full text-center bg-grid font-sevenSegment text-2xl text-accent outline focus:outline-base-100 focus:outline-offset-1 focus:outline-1 focus:border-none border-none focus:ring-0 outline-base-100 outline-offset-1 outline-1 px-2"
+                    />           
+                    <button aria-label="Decrease bar length" onClick={ () => setBar(barLength + 1) } className="px-2 btn btn-ghost btn-pushable outline-neutral border-secondary text-secondary btn-sm rounded-l-none"><BsPlus className="h-4 w-4" /></button>
+                </div>
+            </div>
+            <div className="form-control">
+                <label className="cursor-pointer label" onClick={ toggleMetronome }> 
+                    <span className="label-text">Metronome</span> 
+                    <input                         
+                        aria-label="Metronome sound" 
+                        type="checkbox" 
+                        value="METRONOME_ON"
+                        checked={ metronome }
+                        className="toggle toggle-secondary checked:toggle-accent toggle-sm" 
+                        readOnly
+                    />
+                </label>
+            </div>
+        </div>
+    )
+}
+
+export default Tempo;
